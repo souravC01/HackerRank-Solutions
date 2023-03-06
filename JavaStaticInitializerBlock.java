5c919592c8cf8140649c6dbcd2a73c9f8d54e2ea
sourav <sourav.chandhok@outlook.com>
2023-03-06 00:33:41 -0500

    Java Static Initalizer Block

---
+++ JavaStaticInitializerBlock.java
@@ -0,0 +1,21 @@
+package org.example;
+
+import java.util.Scanner;
+
+public class JavaStaticInitializerBlock {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        int height, breadth;
+
+        breadth = sc.nextInt();
+        height = sc.nextInt();
+
+        if ((breadth <= 0) || (height <= 0)){
+            System.out.println("java.lang.Exception: Breadth and height must be positive");
+            sc.close();
+        }
+        else
+            System.out.println(breadth*height);
+    }
+}
