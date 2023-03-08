f9c1d4c3eecab25dcffb1e19f1a69e364ef11959
sourav <sourav.chandhok@outlook.com>
2023-03-07 20:44:48 -0500

    Pattern Syntax Checker

---
+++ PatternSyntaxChecker.java
@@ -0,0 +1,24 @@
+package org.hackerRank;
+
+import java.util.Scanner;
+import java.util.regex.Pattern;
+import java.util.regex.PatternSyntaxException;
+
+public class PatternSyntaxChecker {
+    public static void main(String[] args){
+        Scanner in = new Scanner(System.in);
+        int testCases = Integer.parseInt(in.nextLine());
+        while(testCases>0 && in.hasNextLine()){
+            String pattern = in.nextLine();
+
+            try {
+                Pattern p = Pattern.compile(pattern);
+                System.out.println("Valid");
+            }
+            catch (PatternSyntaxException e) {
+                System.out.println("Invalid");
+            }
+        }
+        in.close();
+    }
+}
